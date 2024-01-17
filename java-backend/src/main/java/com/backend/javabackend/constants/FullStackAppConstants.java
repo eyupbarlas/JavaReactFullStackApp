c5bedcc56c54d987e35f5e1bd5e083ceddadf3f8
Eyüp Barlas <eyupbarlas2134@gmail.com>
2024-01-17 12:39:23 +0100

    Revert "Adding REST Controllers"

--- java-backend/src/main/java/com/backend/javabackend/constants/FullStackAppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.backend.javabackend.constants;
-
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-public class FullStackAppConstants {
-    public static final String API_V_1_USER = "/api/v1/user";
-}
