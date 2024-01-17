6a553dc090281a0e9b868592767e47ae91076812
Ismayil Mohsumov <ismayil.mohsumov@hp.com>
2024-01-17 13:42:57 +0100

    Adding service class

--- java-backend/src/main/java/com/backend/javabackend/data/User.java
+++ java-backend/src/main/java/com/backend/javabackend/data/User.java
@@ -3,10 +3,16 @@ package com.backend.javabackend.data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
