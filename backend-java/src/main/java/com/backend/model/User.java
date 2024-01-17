9ba9689d5f10697dd0ac244730fac5d5907fe0e7
Ismayil Mohsumov <ismayil.mohsumov@hp.com>
2024-01-17 11:59:46 +0100

    Remove ign

--- backend-java/src/main/java/com/backend/model/User.java
+++
@@ -1,4 +0,0 @@
-package com.backend.model;
-
-public class User {
-}
---
+++ java-backend/src/test/java/com/backend/javabackend/JavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
